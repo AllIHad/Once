c2bde5d3021bd50b4f6d7cedb749f50b883e7fba
ibadurrahmaAlHadi <extremegilang@gmail.com>
2023-09-10 11:15:56 +0700

    login and logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,28 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useContext } from 'react'
+import AuthContext from '../stores/authContext'
 
 export default function Navbar() {
+
+  const { user, login, logout } = useContext(AuthContext)
+  console.log(user)
+
   return (
     <div className="container">
       <nav>
-        <Image src="/Twicelogo.png" width={50} height={50} />
+        <Image alt='logo' src="/Twicelogo.png" width={50} height={50} />
         <h1>Once</h1>
         <ul>
           <li><Link href="/" legacyBehavior><a>Home</a></Link></li>
           <li><Link href="/guides" legacyBehavior><a>Guides</a></Link></li>
+          <p>{}</p>
+          <li onClick={login} className='btn'>Login/Signup</li>
+          <li onClick={logout} className='btn'>Logout</li>
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/Wall.jpg" width={526} height={316} sizes='100vw'/>
+        <Image alt='banner' src="/Wall.jpg" width={526} height={316} sizes='100vw' />
       </div>
     </div>
   )
