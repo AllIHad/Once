8d7bfac053c9cf64086c518a98e6842464cdfc5d
ibadurrahmaAlHadi <extremegilang@gmail.com>
2023-09-12 11:15:01 +0700

    making the tempalte

--- pages/guides.js
+++ pages/guides.js
@@ -1,9 +1,11 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import styles from '../styles/Guides.module.css'
 import authContext from '../stores/authContext'
 
 export default function Guides() {
   const { user, authReady } = useContext(authContext)
+  const { guides, setGuides } = useState(null)
+  const { error, setError } = useState(null)
 
   useEffect(() => {
     if (authReady) {
@@ -12,14 +14,41 @@ export default function Guides() {
           Authorization: 'Bearer ' + user.token.access_token
         }
       })
-        .then(res => res.json())
-        .then(data => console.log(data))
+        .then(res => {
+          if (!res.ok) {
+            throw Error('You must login to be able to see the content')
+          }
+          return res.json()
+        })
+        .then(data => {
+          setGuides(data)
+          setError(null)
+        })
+        .catch(err => {
+          setError(err.message)
+          setGuides(null)
+        })
     }
   }, [user, authReady])
 
   return (
     <div className={styles.guides}>
-      <h2>All Guides</h2>
+      {!authReady && <div>Loading...</div>}
+
+      {error && (
+        <div className={styles.error}>
+          <p>{error}</p>
+        </div>
+      )}
+
+      {guides && guides.map(guide => (
+        <div key={guide.title} className={styles.card} >
+          <h3>{guide.title}</h3>
+          <h4>Written by {guide.author}</h4>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus tempora dignissimos dolorem, omnis magnam, alias quod hic pariatur quo porro ipsa rerum laudantium id! Consequuntur praesentium consectetur alias voluptatum optio quaerat cupiditate. Cupiditate, in. Reprehenderit tempora cumque deleniti maiores tempore earum rem optio quis, iure unde numquam ipsum facilis accusamus.</p>
+        </div>
+      ))}
+
     </div>
   )
 }
